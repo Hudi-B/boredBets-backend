2175e1c58a3cc574013db4a2910f20fafb15134f
RRichard2004 <ricsiruszo90@gmail.com>
2024-03-09 22:22:48 +0100

    update/horse generate
    horse generate,
    links free jockeys to horses,
    if there are not enough jockeys, alerts the admin about the jockey count

--- boredBets/boredBets/Controllers/HorseController.cs
+++ boredBets/boredBets/Controllers/HorseController.cs
@@ -3,6 +3,7 @@ using boredBets.Models.Dtos;
 using boredBets.Repositories.Interface;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace boredBets.Controllers
 {
@@ -12,9 +13,12 @@ namespace boredBets.Controllers
     {
         private readonly IHorseInterface horseInterface;
 
-        public HorseController(IHorseInterface horseInterface)
+        private readonly BoredbetsContext _context;
+
+        public HorseController(IHorseInterface horseInterface, BoredbetsContext context)
         {
             this.horseInterface = horseInterface;
+            _context = context;
         }
 
         [HttpPost("HorsePost")]
@@ -51,6 +55,63 @@ namespace boredBets.Controllers
                 NotFound(); 
             }
             return Ok(result);
+        }
+
+        [HttpPost("GenerateHorses")]
+
+        public async Task<ActionResult<int>> GenerateHorse(int quantity)
+        {
+            var freeJockeys =
+                from jockey in _context.Jockeys
+                where !_context.Horses.Any(horse => horse.JockeyId == jockey.Id)
+                select jockey.Id; //selects free jockey that are not connected to any horse
+
+            if (freeJockeys.Count() >= quantity)
+            {
+                try
+                {
+                    List<string> horseNames = new List<string>();
+
+                    #region ReadFile
+                    string Path = "../../../staticData/maleHorses.txt";
+                    string absolutePath = AppDomain.CurrentDomain.BaseDirectory.ToString() + Path;
+
+                    StreamReader sr = new StreamReader(absolutePath);
+                    while (!sr.EndOfStream)
+                    {
+                        horseNames.Add(sr.ReadLine());
+                    }
+                    #endregion
+
+                    Random random = new Random();
+                    for (int i = 0; i < quantity; i++)
+                    {
+                        var newHorse = new Horse
+                        {
+                            Id = Guid.NewGuid(),
+                            Name = horseNames[random.Next(horseNames.Count)],
+                            Age = random.Next(4) + 2,
+                            Stallion = random.Next(2) == 0,
+                            JockeyId = freeJockeys.ToList()[i]
+                        };
+
+                        await _context.Horses.AddAsync(newHorse);
+                    }
+
+                    await _context.SaveChangesAsync();
+
+                    return StatusCode(201);
+                }
+                catch (Exception ex)
+                {
+                    return StatusCode(500, ex);
+                }
+            }
+            else
+            {
+                return StatusCode(500, "Jockey quantity not sufficient. Available jockeys: "+freeJockeys.Count());
+            }
+
         }
 
 
