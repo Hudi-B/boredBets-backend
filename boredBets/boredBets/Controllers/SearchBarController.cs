22ecfc347cf3df4a1f2de7038bedb9446afdd1eb
RRichard2004 <ricsiruszo90@gmail.com>
2024-03-28 20:52:50 +0100

    update/ optimized searchbar
    searchbar faster, also returns only significant data

--- boredBets/boredBets/Controllers/SearchBarController.cs
+++ boredBets/boredBets/Controllers/SearchBarController.cs
@@ -7,9 +7,7 @@ namespace boredBets.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-
     
-
     public class SearchBarController : ControllerBase
     {
         public class AllData
@@ -18,32 +16,31 @@ namespace boredBets.Controllers
             public List<User> users = new List<User>();
             public List<Jockey> jockeys = new List<Jockey>();
 
-            public void AddHorse(Horse horse)
+            public AllData(List<Horse> horses, List<User> users, List<Jockey> jockeys)
             {
-                horses.Add(horse);
-            }
-
-            public void AddUser(User user)
-            {
-                users.Add(user);
-            }
-
-            public void AddJockey(Jockey jockey)
-            {
-                jockeys.Add(jockey);
+                this.horses = horses;
+                this.users = users;
+                this.jockeys = jockeys;
             }
 
             public List<object> GetPaginatedAllData(int page, int perPage)
             {
-                List<object> allData = new List<object>();
-                allData.AddRange(horses.Select(h => new { Type = "Horse", Data = h }));
-                allData.AddRange(users.Select(u => new { Type = "User", Data = u }));
-                allData.AddRange(jockeys.Select(j => new { Type = "Jockey", Data = j }));
+                var allData = new List<object>();
+
+                allData.AddRange(horses.Select(h => new { Type = "Horse", Data = new HorseData(h.Id, h.Name, h.Stallion) }));
+                allData.AddRange(users.Select(u => new { Type = "User", Data = new UserData(u.Id, u.Username) }));
+                allData.AddRange(jockeys.Select(j => new { Type = "Jockey", Data = new JockeyData(j.Id, j.Name, j.Male, j.Horses.Any()) }));
 
                 int startIndex = (page - 1) * perPage;
 
-                var search = allData.Skip(startIndex).Take(perPage).ToList();
 
+                allData = allData.OrderBy(item =>
+                {
+                    dynamic d = item;
+                    return d.Data.Name.ToString().ToLower();
+                }).ToList();
+
+                var search = allData.Skip(startIndex).Take(perPage).ToList();
                 return search;
             }
 
@@ -57,27 +54,12 @@ namespace boredBets.Controllers
 
         [HttpGet]
         public async Task<ActionResult> Get(int page = 1, int perPage = 60)
-        {
-            var horses = await _context.Horses.ToListAsync(); 
-            var users = await _context.Users.ToListAsync();
-            var jockeys = await _context.Jockeys.ToListAsync();
-
-            AllData allData = new AllData();
-
-            foreach (var horse in horses)
-            {
-                allData.AddHorse(horse);
-            }
-
-            foreach (var user in users)
-            {
-                allData.AddUser(user);
-            }
-
-            foreach (var jockey in jockeys)
-            {
-                allData.AddJockey(jockey);
-            }
+        { 
+            AllData allData = new AllData(
+                await _context.Horses.ToListAsync(),
+                await _context.Users.ToListAsync(),
+                await _context.Jockeys.ToListAsync()
+                );
 
             var paginatedData = allData.GetPaginatedAllData(page, perPage);
 
@@ -85,4 +67,9 @@ namespace boredBets.Controllers
         }
     }
 
+    internal record HorseData(Guid Id, string? Name, bool Stallion);
+
+    internal record UserData(Guid Id, string? Name);
+
+    internal record JockeyData(Guid Id, string? Name, bool Male, bool HasHorse);
 }
