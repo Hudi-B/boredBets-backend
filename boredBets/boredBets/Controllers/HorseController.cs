34e9bbaf85d142c1c621472e5cea844339e108e3
RRichard2004 <ricsiruszo90@gmail.com>
2024-04-07 22:40:49 +0200

    feature/generate horse calls generate jockey

--- boredBets/boredBets/Controllers/HorseController.cs
+++ boredBets/boredBets/Controllers/HorseController.cs
@@ -4,6 +4,7 @@ using boredBets.Repositories.Interface;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Net.Http.Headers;
 
 namespace boredBets.Controllers
 {
@@ -73,6 +74,8 @@ namespace boredBets.Controllers
             return Ok(result);
         }
 
+
+
         [HttpPost("GenerateHorses")]
 
         public async Task<ActionResult<int>> GenerateHorse(int quantity)
@@ -82,81 +85,120 @@ namespace boredBets.Controllers
                 where !_context.Horses.Any(horse => horse.JockeyId == jockey.Id)
                 select jockey.Id; //selects free jockey that are not connected to any horse
 
-            if (freeJockeys.Count() >= quantity)
+            bool refreshList = false;
+
+            if (freeJockeys.Count() < quantity)
             {
-                try
+                using (var client = new HttpClient())
                 {
-                    List<string> maleHorseName = new List<string>();
-                    List<string> femaleHorseName = new List<string>();
-                    List<string> Countries = new List<string>();
 
-                    #region ReadFile
+                    client.DefaultRequestHeaders.Accept.Clear();
+                    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 
-
-                    string staticData = AppDomain.CurrentDomain.BaseDirectory.ToString() + "../../../staticData/";
-
-                    StreamReader sr;
-                    sr = new StreamReader(staticData + "maleHorses.txt");
-                    while (!sr.EndOfStream)
+                    try
                     {
-                        maleHorseName.Add(sr.ReadLine());
-                    }
-                    sr = new StreamReader(staticData + "femaleHorses.txt");
-                    while (!sr.EndOfStream)
-                    {
-                        femaleHorseName.Add(sr.ReadLine());
-                    }
-                    sr = new StreamReader(staticData + "countries.txt");
-                    while (!sr.EndOfStream)
-                    {
-                        Countries.Add(sr.ReadLine());
-                    }
-                    sr.Close();
-                    #endregion
-
-                    Random random = new Random();
-                    for (int i = 0; i < quantity; i++)
-                    {
-                        bool male = random.Next(2) == 0;
-                        string name;
-                        if (male)
-                        { 
-                            name = maleHorseName[random.Next(maleHorseName.Count())];
+                        StringContent httpContent = new StringContent("");
+                        string fullUrl = $"https://boredbetsapidev.azurewebsites.net/api/Jockey/GenerateJockey?quantity=" + (quantity - freeJockeys.Count());
+                        HttpResponseMessage response = await client.PostAsync(fullUrl, httpContent);
+                        if (response.IsSuccessStatusCode)
+                        {
+                            string responseContent = await response.Content.ReadAsStringAsync();
+                            // Parse the responseContent to update your list
+                            refreshList = true;
                         }
                         else
                         {
-                            name = femaleHorseName[random.Next(femaleHorseName.Count())];
+                            return StatusCode(500,$"Error getting Jockeys: {response.StatusCode}");
                         }
-                        var newHorse = new Horse
-                        {
-                            Id = Guid.NewGuid(),
-                            Name = name,
-                            Age = random.Next(4) + 2,
-                            Country = Countries[random.Next(Countries.Count())],
-                            Stallion = male,
-                            JockeyId = freeJockeys.ToList()[i]
-                        };
-
-                        await _context.Horses.AddAsync(newHorse);
                     }
-
-                    await _context.SaveChangesAsync();
-
-                    return StatusCode(201);
-                }
-                catch (Exception ex)
-                {
-                    return StatusCode(500, ex);
+                    catch (HttpRequestException ex)
+                    {
+                        return StatusCode(500, "HTTP Request Error: {ex.Message}");
+                    }
+                    catch (Exception ex)
+                    {
+                        return StatusCode(500, $"General Exception: {ex.Message}");
+                    }
                 }
             }
-            else
+            if (refreshList)
             {
-                return StatusCode(500, "Jockey quantity not sufficient. Available jockeys: "+freeJockeys.Count());
+                freeJockeys =
+                    from jockey in _context.Jockeys
+                    where !_context.Horses.Any(horse => horse.JockeyId == jockey.Id)
+                    select jockey.Id;
             }
 
+
+            try
+            {
+                List<string> maleHorseName = new List<string>();
+                List<string> femaleHorseName = new List<string>();
+                List<string> Countries = new List<string>();
+
+                #region ReadFile
+
+
+                string staticData = AppDomain.CurrentDomain.BaseDirectory.ToString() + "../../../staticData/";
+
+                StreamReader sr;
+                sr = new StreamReader(staticData + "maleHorses.txt");
+                while (!sr.EndOfStream)
+                {
+                    maleHorseName.Add(sr.ReadLine());
+                }
+                sr = new StreamReader(staticData + "femaleHorses.txt");
+                while (!sr.EndOfStream)
+                {
+                    femaleHorseName.Add(sr.ReadLine());
+                }
+                sr = new StreamReader(staticData + "countries.txt");
+                while (!sr.EndOfStream)
+                {
+                    Countries.Add(sr.ReadLine());
+                }
+                sr.Close();
+                #endregion
+
+                Random random = new Random();
+                int maleHorseNameCount = maleHorseName.Count();
+                int femaleHorseNameCount = femaleHorseName.Count();
+                int countriesCount = Countries.Count();
+                var freeJockeyIds = freeJockeys.ToList();
+
+                for (int i = 0; i < quantity; i++)
+                {
+                    bool male = random.Next(2) == 0;
+                    string name;
+                    if (male)
+                    {
+                        name = maleHorseName[random.Next(maleHorseNameCount)];
+                    }
+                    else
+                    {
+                        name = femaleHorseName[random.Next(femaleHorseNameCount)];
+                    }
+                    var newHorse = new Horse
+                    {
+                        Id = Guid.NewGuid(),
+                        Name = name,
+                        Age = random.Next(4) + 2,
+                        Country = Countries[random.Next(countriesCount)],
+                        Stallion = male,
+                        JockeyId = freeJockeyIds[i]
+                    };
+
+                    await _context.Horses.AddAsync(newHorse);
+                }
+
+                await _context.SaveChangesAsync();
+
+                return StatusCode(201);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex);
+            }
         }
-
-
-
     }
 }
