87cb887c4c9b559c747343555ebf87ab1b2132e9
bag0ly <balazsforgo98@gmail.com>
2024-04-24 10:44:21 +0200

    fix/Notification endpoints

--- boredBets/boredBets/Repositories/NotificationsService.cs
+++ boredBets/boredBets/Repositories/NotificationsService.cs
@@ -17,7 +17,7 @@ namespace boredBets.Repositories
         {
             var notifications = await _context.Notifications.Where(n=>n.UserId==UserId).OrderBy(n => n.Created).ToListAsync();
 
-            if (notifications.Count()>0)
+            if (!notifications.Any())
             {
                 return null;
             }
@@ -29,7 +29,7 @@ namespace boredBets.Repositories
         {
             var notifications = await _context.Notifications.Where(n=>n.UserId==UserId && n.Seen == false).OrderBy(n=>n.Created).ToListAsync();
 
-            if (notifications.Count()>0) 
+            if (!notifications.Any()) 
             {
                 return null;
             }
