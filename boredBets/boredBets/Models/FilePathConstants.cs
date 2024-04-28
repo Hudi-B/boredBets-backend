ca0437568486c706f6de93c5f52107063c90b4cb
bag0ly <balazsforgo98@gmail.com>
2024-04-28 13:22:44 +0200

    feature/added FilePathConstants
    for constant file paths

---
+++ boredBets/boredBets/Models/FilePathConstants.cs
@@ -0,0 +1,22 @@
+﻿namespace boredBets.Models
+{
+    public class FilePathConstants
+    {
+        public static readonly string StaticData = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..", "..", "..", "staticData");
+
+        // Common files
+        public static readonly string FamilyNames = Path.Combine(StaticData, "familyNames.txt");
+        public static readonly string MaleNames = Path.Combine(StaticData, "maleNames.txt");
+        public static readonly string FemaleNames = Path.Combine(StaticData, "femaleNames.txt");
+        public static readonly string MaleMiddleNames = Path.Combine(StaticData, "maleMiddleNames.txt");
+        public static readonly string FemaleMiddleNames = Path.Combine(StaticData, "femaleMiddleNames.txt");
+        public static readonly string Countries = Path.Combine(StaticData, "countries.txt");
+
+        // Specific files
+        public static readonly string Tracks = Path.Combine(StaticData, "trackNames.txt");
+        public static readonly string MaleHorses = Path.Combine(StaticData, "maleHorses.txt");
+        public static readonly string FemaleHorses = Path.Combine(StaticData, "femaleHorses.txt");
+        public static readonly string HorseFirst = Path.Combine(StaticData, "horseFirst.txt");
+        public static readonly string HorseSecond = Path.Combine(StaticData, "horseSecond.txt");
+    }
+}
